876a8bbe07f4cbc2251f3e2f178a1dfdc99cb75d
MinnieVB <big.nomes@gmail.com>
2013-09-26 12:15:07 +0930

    Paper progress and initial graph for missing positions
    Paper: abstract, conclusion, related work.
    
    Analysis: histogram of positions of missing bits.

---
+++ data/dist_missing_bits.R
@@ -0,0 +1,8 @@
+#Set working directory
+setwd('~/Desktop/paper-1/data')
+
+#Read in the data
+missing_pos = read.table('MISSED.txt', header=T)
+
+attach(missing_pos)
+hist(missing_pos$positions, breaks = 20, freq = FALSE, right = TRUE, density = 10, angle = 45, col = 'BLUE', border = NULL, main = paste("Histogram of Positions of missing bits"), xlab = "Positions of missing bits")
