7a22eb6be1c5fcdfd4ab91d75438c8d9ffd99449
MinnieVB <big.nomes@gmail.com>
2013-09-30 15:38:44 +0930

    Work on conclusion

--- data/dist_missing_bits.R
+++ data/dist_missing_bits.R
@@ -5,4 +5,4 @@ setwd('~/Desktop/paper-1/data')
 missing_pos = read.table('MISSED.txt', header=T)
 
 attach(missing_pos)
-hist(missing_pos$positions, breaks = 20, freq = FALSE, right = TRUE, density = 10, angle = 45, col = 'BLUE', border = NULL, main = paste("Histogram of Positions of missing bits"), xlab = "Positions of missing bits")
+hist(missing_pos$positions, breaks = 30, freq = FALSE, right = TRUE, density = 10, angle = 45, col = 'BLUE', border = NULL, main = paste("Histogram of Positions of missing bits"), xlab = "Positions of missing bits")
